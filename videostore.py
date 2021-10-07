712340781a6c81e300a1feb5f20b5565dcf2dbe1
Skycloudd <benkrassenburg@gmail.com>
2021-10-07 20:12:15 +0200

    add individual product page

--- videostore.py
+++ videostore.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Flask, render_template
+from flask import Flask, abort, render_template
 
 from database_utils import PRODUCTS_DATABASE_FILENAME, setup_databases
 from product import Product
@@ -30,3 +30,23 @@ def products():
     ]
 
     return render_template("products.html", products=products)
+
+
+@app.route("/products/<int:product_id>")
+def view_product(product_id):
+    with PRODUCTS_DATABASE_FILENAME.open("r") as f:
+        data = json.load(f)
+
+    product = next(
+        (product for product in data["products"] if product["id"] == product_id), None
+    )
+
+    if product is None:
+        abort(404)
+
+    return render_template("view_product.html", product=product)
+
+
+@app.route("/buy")
+def buy():
+    abort(501)
