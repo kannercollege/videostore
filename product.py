645bacead342bd550eeb8a929b005bc0ec17fb62
Skycloudd <benkrassenburg@gmail.com>
2021-10-08 18:41:30 +0200

    make product __init__() automatically round price

--- product.py
+++ product.py
@@ -2,5 +2,5 @@ class Product:
     def __init__(self, id: int, name: str, price: float, image_filename: str):
         self.id = id
         self.name = name
-        self.price = price
+        self.price = round(price, 2)
         self.image_filename = image_filename
