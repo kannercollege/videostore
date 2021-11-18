8e0c2c216f88bd2efd439867a8f8088c4448d327
Skycloudd <benkrassenburg@gmail.com>
2021-11-18 21:25:31 +0100

    implement buying products

--- videostore/schema.sql
+++ videostore/schema.sql
@@ -2,6 +2,8 @@ DROP TABLE IF EXISTS user;
 
 DROP TABLE IF EXISTS product;
 
+DROP TABLE IF EXISTS product_order;
+
 CREATE TABLE user (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT UNIQUE NOT NULL,
@@ -14,4 +16,19 @@ CREATE TABLE product (
     product_name TEXT NOT NULL,
     product_description TEXT,
     price REAL NOT NULL
+);
+
+CREATE TABLE product_order (
+    id integer PRIMARY KEY AUTOINCREMENT,
+    customer_id INTEGER NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    country TEXT NOT NULL,
+    city TEXT NOT NULL,
+    street TEXT NOT NULL,
+    house_number TEXT NOT NULL,
+    zip_code TEXT NOT NULL,
+    email TEXT NOT NULL,
+    full_name TEXT NOT NULL,
+    phone_number TEXT NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES user (id) ON UPDATE CASCADE ON DELETE CASCADE
 );
--- videostore/store.py
+++ videostore/store.py
@@ -26,7 +26,7 @@ def test():
     return "test"
 
 
-def get_product(id, check_admin=True):
+def get_product(id):
     product = (
         get_db()
         .execute(
@@ -39,12 +39,66 @@ def get_product(id, check_admin=True):
     )
 
     if product is None:
-        abort(404, f"Product id {id} doesn't exist.")
-
-    if check_admin and not flask_login.current_user.is_admin:
-        abort(403)
+        abort(404)
 
     return product
+
+
+@bp.route("/<int:id>/view")
+def view(id):
+    product = get_product(id)
+
+    return render_template("store/view.html", product=product)
+
+
+@bp.route("/<int:id>/buy", methods=("GET", "POST"))
+@flask_login.login_required
+def buy(id):
+    if request.method == "POST":
+        country = request.form["country"]
+        city = request.form["city"]
+        street = request.form["street"]
+        house_number = request.form["house_number"]
+        zip_code = request.form["zip_code"]
+
+        email = request.form["email"]
+        full_name = request.form["full_name"]
+        phone_number = request.form["phone_number"]
+
+        username = flask_login.current_user.get_id()
+
+        db = get_db()
+        user = db.execute(
+            "SELECT * FROM user WHERE username = ?", (username,)
+        ).fetchone()
+
+        error = None
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                "INSERT INTO product_order (customer_id, country, city, street, house_number, zip_code, email, full_name, phone_number) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                (
+                    user["id"],
+                    country,
+                    city,
+                    street,
+                    house_number,
+                    zip_code,
+                    email,
+                    full_name,
+                    phone_number,
+                ),
+            )
+            db.commit()
+
+            return redirect(url_for("store.view", id=id))
+
+    product = get_product(id)
+
+    return render_template("store/buy.html", product=product)
 
 
 @bp.route("/create", methods=("GET", "POST"))
@@ -67,12 +121,13 @@ def create():
             flash(error)
         else:
             db = get_db()
-            db.execute(
+            cursor = db.execute(
                 "INSERT INTO product (product_name, product_description, price) VALUES (?, ?, ?)",
                 (product_name, product_description, price),
             )
             db.commit()
-            return redirect(url_for("store.index"))
+
+            return redirect(url_for("store.view", id=cursor.lastrowid))
 
     return render_template("store/create.html")
 
