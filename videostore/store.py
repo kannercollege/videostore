ff1feaac87d4ac2d3fbf561140e1774a82c9e526
Skycloudd <51929172+Skycloudd@users.noreply.github.com>
2022-02-20 16:04:41 +0100

    add image thing which doesnt work yet and add prices back

--- videostore/store.py
+++ videostore/store.py
@@ -7,11 +7,24 @@ bp = Blueprint("store", __name__)
 async def get_data(url):
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
-            data = await response.json()
+            try:
+                data = await response.json()
+            except aiohttp.ContentTypeError:
+                # oracle returns an html page if the id is invalid
+                return None
     return data
 
 
+# define url constants
+
 MOVIES_URL = "https://apex.oracle.com/pls/apex/lkc-ct14/store/movies"
+
+
+def MOVIE_IMAGE_URL(id):
+    return f"https://apex.oracle.com/pls/apex/lkc-ct14/store/images/{id}"
+
+
+# define routes
 
 
 @bp.route("/")
@@ -32,15 +45,29 @@ async def all():
     return render_template("store/all.html", products=products)
 
 
+# id = imdb id
+# i dont recieve any other usable id from the api
+#
+# the index doesnt seem to relate to the id either
 async def get_product(id):
     data = await get_data(MOVIES_URL)
 
-    products = data["items"]
-
-    for product in products:
+    for product in data["items"]:
         if product["imdb"] == id:
             return product
     abort(404)
+
+
+# i have to somehow get the correct product id, which i dont know how to,
+# since i only ever recieve the imdb id,
+# but that means i can never use the image endpoint
+# since it requires the actual id instead of the imdb id
+# pain.
+async def get_product_image(id):
+    data = await get_data(MOVIE_IMAGE_URL(id))
+
+    # returns None if there's no image
+    return data.get("image")
 
 
 @bp.route("/<id>/view")
@@ -48,7 +75,15 @@ async def view(id):
     product = await get_product(id)
     genres = product["genres"].split(",")
 
-    return render_template("store/view.html", product=product, genres=genres)
+    # we dont even use the correct id here
+    # so this will always be None
+    # but if i ever get the correct id,
+    # this should work
+    image_b64 = await get_product_image(id)
+
+    return render_template(
+        "store/view.html", product=product, genres=genres, image_b64=image_b64
+    )
 
 
 @bp.route("/search")
